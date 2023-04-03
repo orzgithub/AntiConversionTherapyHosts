45bee9476c6ca17c514c7d8d61f680e8879a2309
orzgithub <nomail@qq.com>
2023-04-03 12:12:34 +0800

    首次提交。感谢@FuncSir的工作。

---
+++ makeit.sh
@@ -0,0 +1,58 @@
+#/bin/bash
+## 使用提示
+if [[ ($# == 0) || ($1 == "-h") || ($1 == "--help") ]]; then
+	echo "脚本使用帮助"
+	echo -e "$0 [参数] [路径]"
+	echo -e "参数列表："
+	echo -e "--help\t-h\t查看这条帮助"
+	echo -e "路径说明："
+	echo -e "请输入机构列表的下载链接（请使用CSV-PLAIN的链接）。或者当前目录如果存在source.txt，可以输入local以使用该文件。"
+	exit 0
+fi
+## 下载机构列表
+# 从传入的参数所指定的链接下载机构列表，用于后续处理。传入参数为local则使用当前目录下的source.txt。
+if [ $1 == "local" ]; then
+	if [ ! -f source.txt ]; then
+		echo "当前目录不存在名为source.txt的文件。转换已终止。"
+		exit 1
+	fi
+else
+	donloadnum = 0
+	while [ -f source${donloadnum}.txt ]
+	do
+		((donloadnum++))
+	done
+	echo -e "下载机构列表中"
+	wget -O source${donloadnum}.txt $1
+	if [ $? == 0 ]; then
+		echo -e "下载完成"
+		if [ -f source.txt ]; then
+			rm source.txt
+		fi
+		mv source${donloadnum}.txt source.txt
+	else
+		echo -e "下载失败，转换已终止。"
+		rm source${donloadnum}.txt
+		exit 1
+	fi
+fi
+## 生成host文件
+# 该方式产生的host文件可以对对应站点进行屏蔽。但由于部分该类机构采用在微博宣传、与公共网站（也可能是作为机构伪装的普通公司）合作宣传等方式，若屏蔽会牵连其本身所在公共站点而影响使用和降低隐蔽性，故加入白名单，不作屏蔽。
+# 站点白名单。请使用"(站点1域名)|(站点2域名)|…|(站点n域名)"的格式编写。
+host_whitelist = "(weibo.com)|(jinan.dzwww.com)|(www.jjerw.com)|(www.qianxuew.com)"
+echo "生成HOST中…"
+cat source.txt | awk -F";" '{ print $3}' | grep -oaE "http[s]?://[a-zA-Z0-9.-]+" | awk -F"/" '{ print $3}' | grep -vaE ${host_whitelist} | sed "s/^/127.0.0.1\t&/g" > hostfile.txt
+echo "HOST已保存为hostfile.txt。"
+host_whitelist = "(weibo.com)|(jinan.dzwww.com)|(www.jjerw.com)|(www.qianxuew.com)"
+# 该方式产生的host文件可以对对应站点进行屏蔽。对于借助其它网站建立自己宣传站点的机构也予以屏蔽，会造成其所在站点本身也被阻止而无法访问。微博等常用站点不进行屏蔽以减少痕迹。
+# 站点白名单。请使用"(站点1域名)|(站点2域名)|…|(站点n域名)"的格式编写。
+host_whitelist_strict="(weibo.com)"
+echo "生成更严格的HOST中…"
+cat source.txt | awk -F";" '{ print $3}' | grep -oaE "http[s]?://[a-zA-Z0-9.-]+" | awk -F"/" '{ print $3}' | grep -vaE ${host_whitelist_strict} | sed "s/^/127.0.0.1\t&/g" > hostfile_strict.txt
+echo "严格的HOST已保存为hostfile_strict.txt。"
+## 生成ABP规则
+# 该方式产生的ABP规则可以对对应站点和页面进行屏蔽，可以针对特定的网站地址进行屏蔽。但该类规则需要搭配浏览器支持使用，比如使用AdGuard、ADBlock Plus插件的Chrome、Firefox、Safari，或者带有内建广告屏蔽规则支持的浏览器等。AdGuardHome建议使用HOST，其使用DNS拦截的机制，无法使用该规则中的页面拦截。
+# 建议和HOST一起使用，作为HOST的补充。uBlock Origin对于域名拦截有直接的提示且不会强制阻止。其余拦截程序也表现不佳，不能完全阻止对站点访问。
+echo "生成ABP规则中…"
+cat source.txt | awk -F";" '{ print $3}' | grep -oaE "http[s]?://[a-zA-Z0-9./-]+" | sed "s|^http://||g" | sed "s|^https://||g" | sed "s|/$||g" | sed "s/^/||/g" | sed "s/$/^/g" > abprules.txt
+echo "规则已保存为abprules.txt。"
---
+++ readme.md
@@ -0,0 +1,28 @@
+# “戒网瘾”（可能含扭转机构）机构列表屏蔽规则转换
+
+## 说明
+这是一个可以将推特用户[@FuncSir](https://twitter.com/FuncSir "@FuncSir的推特主页")所制作的 [“戒网瘾”（可能含扭转机构）机构列表](https://twitter.com/FuncSir/status/1640740300086714373 "推文链接")当中对应机构的官网转换成屏蔽列表的简单的Shell脚本。感谢他的努力。这份列表是为了所有的人，希望不再有人被送进那样糟糕的披着“戒网瘾”、“扭转治疗”的违法机构。虽然可能这样的建议不太道德，但如果认为自己父母有把自己送进去的风险，还是找机会接触到他们的手机，把这样的机构官网预先屏蔽掉比较好，这是保护自己的重要方式。
+
+## 使用方法
+进入一个类Unix系统，需要系统支持wget、awk、grep、sed指令。绝大多数操作系统应该已经内置了这些东西。同时Windows下可以选择使用MSYS，MSYS2，Cygwin或WSL执行该脚本。<br>
+```
+脚本使用帮助：
+./makeit.sh [参数] [路径]
+参数列表：
+--help  -h      查看这条帮助
+路径说明：
+请输入机构列表的下载链接（请使用CSV-PLAIN的链接）。或者当前目录如果存在source.txt，可以将路径输入为local以使用该文件。
+```
+
+## 屏蔽列表的说明
+该脚本会产生三个文件，hostfile.txt、hostfile_strict.txt、abprules.txt。作用如下：<br>
+hostfile.txt：生成的一个hosts文件。这个版本只屏蔽了域名即其官网的类型，而部分机构将其官网设置在其它网站之内（例如济南大众网下藏着济南市宏开教育培训学校的官网，如果屏蔽机构官网则济南大众网也会一起屏蔽。），由此影响正常网站（虽然在底下还藏这种机构官网，想必这些主站也不会是无辜的“正常网站吧”…）的访问，为避免这样的影响未对此类网站作出屏蔽。<br>
+hostfile_strict.txt：一个更加严格版本的hosts文件，这个版本屏蔽了上述将官网设置在其它网站之内情况的域名，以达到更好的屏蔽效果。但有部分机构将宣传仅设置在微博等平台上，由于该类平台较为常用，屏蔽后较影响正常使用，不作处理。<br>
+abprules.txt：用于ADBlock Plus等类似广告屏蔽工具的规则，对所有的站点进行针对性屏蔽，但需要浏览器支持，不能对设备全局生效。另外似乎广告屏蔽插件效果表现不佳，很多页面仍能加载出一小部分，表现出布局混乱，有时甚至完全不起作用。建议搭配hosts文件使用。<br>
+
+## 使用方式
+hosts文件：<br>
+Windows系统的hosts位于``C:\Windows\System32\drivers\etc\hosts``，MacOS和Linux系统的hosts位于``/etc/hosts``，Android系统的hosts位于``/system/etc/hosts``。将生成的hosts文件中的内容复杂并粘贴于对应系统hosts文件尾部即可。不过由于权限问题，对于没有了解的人可能有些难度，在此不适合拿出大篇幅讲述权限问题的处理。具体可以搜索了解。<br>
+AdBlock Plus规则：
+可以在浏览器的插件商店中找到诸如AdBlock Plus、uBlock Origin、AdGuard等广告屏蔽插件，其中有设置自定义规则的地方可供输入，将生成的规则复制进去即可。或者，本仓库依托于GitHub提供一个规则订阅，地址为：[https://github.com/orzgithub/AntiConversionTherapyHosts/raw/main/adblock-plus.txt](https://github.com/orzgithub/AntiConversionTherapyHosts/raw/main/adblock-plus.txt)。考虑到国内网络环境，提供一个由[ghproxy](https://ghproxy.com)提供的代理加速链接，取决于代理的服务提供稳定性，该链接不保证持续可用。[https://ghproxy.com/https://raw.githubusercontent.com/orzgithub/AntiConversionTherapyHosts/main/adblock-plus.txt](https://ghproxy.com/https://raw.githubusercontent.com/orzgithub/AntiConversionTherapyHosts/main/adblock-plus.txt)。将订阅规则添加到规则订阅即可。<br>
+考虑到在手机上使用的不便性，或许需要有志愿者建设一个ADGuardHome服务器或类似的东西，将该服务器设定为设备使用的私人DNS服务器即可起到等同于HOSTS的功能。限于自己条件不足，仅能在此提起建议。<br>
